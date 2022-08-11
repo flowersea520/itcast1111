683d8a1d39c9bedbd99d98c278839506fd23d864
flowersea <2894266590@qq.com>
2022-08-12 00:50:22 +0800

    master写的一行代码

--- src/main/java/controller/TestController2.java
+++ src/main/java/controller/TestController2.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController2 {
     public static void main(String[] args) {
+        System.out.println("我master分支写的代码");
         System.out.println("我爱你");
     }
 }
