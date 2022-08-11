5d6d47b3ed8fd0f91eaa77ce83abbb20f6b4a14f
flowersea <2894266590@qq.com>
2022-08-12 01:02:41 +0800

    main分支加了一行注释

--- src/main/java/controller/TestController2.java
+++ src/main/java/controller/TestController2.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController2 {
     public static void main(String[] args) {
         System.out.println("我master分支写的代码");
+
+        // main加了一行注释
+
+
         System.out.println("我爱你");
     }
 }
