d2c5317eb0a00251282fa9a094dbcd0618e3b435
flowersea <2894266590@qq.com>
2022-08-12 07:06:56 +0800

    会冲突吗

--- src/main/java/controller/TestController3.java
+++ src/main/java/controller/TestController3.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController3 {
 
-    // master分支更新，尝试push到main分支，然后修改main分支的相同文件commit，然后再使用git pull拉取，
+    // master分支更新，尝试push到main分支，
 
 }
