df4cdc7735488eb18cee44bdd7f4d2012698243c
flowersea <2894266590@qq.com>
2022-08-12 06:46:43 +0800

    会冲突

--- src/main/java/controller/TestController3.java
+++ src/main/java/controller/TestController3.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController3 {
-    // master分支更新
+
+    // master分支更新，尝试push到main分支，然后修改main分支的相同文件commit，然后再使用git pull拉取，
 
 }
